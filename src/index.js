db14edc619d902fe5aea3d50720ea5c80548aba7
kodenow <111261247+kodenow@users.noreply.github.com>
2022-11-25 16:27:03 +0800

    more css changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import "./index.css";
 
 //this is going to be rendered in index.html div.root
 ReactDOM.render(<App />, document.getElementById("root"));
