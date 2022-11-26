63a781754e1c642ffd5edfc211c078099d6d1e3b
kodenow <111261247+kodenow@users.noreply.github.com>
2022-11-26 08:45:48 +0800

    Nav  section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,25 @@ import logo from "../../assets/logo.svg";
 //BEM => Block Element Modifier, a css naming convention for classes
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
-
+  const Menu = () => (
+    <>
+      <p>
+        <a href="#Home">Home</a>
+      </p>
+      <p>
+        <a href="#wgpt3">What is GPT?</a>
+      </p>
+      <p>
+        <a href="#possibility">Open AI</a>
+      </p>
+      <p>
+        <a href="#features">Case Studies</a>
+      </p>
+      <p>
+        <a href="#blog">Library</a>
+      </p>
+    </>
+  );
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -13,41 +31,39 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <p>
-            <a href="#Home">Home</a>
-          </p>
-          <p>
-            <a href="#wgpt3">What is GPT?</a>
-          </p>
-          <p>
-            <a href="#possibility">Open AI</a>
-          </p>
-          <p>
-            <a href="#features">Case Studies</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
+          <Menu />
         </div>
-        <div className="gpt3__navbar-sign">
-          <p>Sign in</p>
-          <button type="button">Sign up</button>
-        </div>
-        <div className="gpt3__navbar-menu">
-          {toggleMenu ? (
-            <RiCloseLine
-              color="#fff"
-              size={27}
-              onClick={() => setToggleMenu(false)}
-            />
-          ) : (
-            <RiMenu3Line
-              color="#fff"
-              size={27}
-              onClick={() => setToggleMenu(true)}
-            />
-          )}
-        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="gpt3__header section__padding" id="Home">
+      <div className="gptc__header-content">
+        <h1 className="gradient__text">
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your email address" />
+          buttom
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
